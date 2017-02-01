c5f6390d32b574c44c67c336ef125e30dc63d97c
Erratum <lamiethno@gmail.com>
2017-02-01 12:32:51 +0100

    DUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUURH!

---
+++ APPREF/TP2/src/exo1/AfficheEntete.java
@@ -0,0 +1,28 @@
+package exo1;
+
+public class AfficheEntete {
+	// ce programme charge une charge à partir de son nom et affiche l'entete de cette classe
+	// public abstract class etc...
+	// le paramétrage des types n'est pas affiché
+	// à faire dans une future version : idem pour chaque méthode
+
+	
+	public static void main(String[] args) {
+		// à tester avec différents types de java.util
+		Class <?> classe = java.util.List.class;
+		
+		System.out.println(enTete(classe));
+	}
+
+	private static String enTete(Class<?> classe) {
+		String s = classe.toGenericString();
+		if(classe.getSuperclass()!=null) s+=" extends "+classe.getSuperclass().getSimpleName();
+		if(classe.getInterfaces().length>0){
+			if(classe.isInterface()) s+=" extends "; else s += " implements ";
+			for(Class<?> c:classe.getInterfaces()) s+=c.getSimpleName()+",";
+			s=s.substring(0, s.length()-1);
+		}
+		return s;
+	}
+
+}
